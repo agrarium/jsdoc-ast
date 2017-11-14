fc7d98564e6d5464d9e433e5ed24247a431b5723
awinogradov <winogradovaa@gmail.com>
2017-11-14 22:40:28 +0300

    traverse in context

--- test.js
+++ test.js
@@ -6,9 +6,11 @@ const source = `
  * @param hack
  * 
 */
-module.exports = function test(hack) {};
+module.exports = function test(hack) {
+    let j = 0;
+};
 `;
 
 const ast = jsDocAst(source, './test.js');
 
-console.log(ast);
+console.log(JSON.stringify(ast));
